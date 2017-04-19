c78e5857e28c67e46b210ce17cca5a3e74a89b54
ramindel22 <staroo_2@yahoo.com>
2017-04-19 05:35:12 +0430

    Update Bdelete.sh

--- Bdelete.sh
+++ Bdelete.sh
@@ -3,72 +3,93 @@
 cd $HOME/Bdelete
 
 install() {
-	    cd tg
-		sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-		sudo apt-get install g++-4.7 c++-4.7
-		sudo apt-get update		
-        sudo apt-get upgrade
-		sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-		sudo apt-get install lua-lgi
-		sudo apt-get install libnotify-dev
-		sudo apt-get install screen
-		sudo apt-get install tmux
+	    cd libs
+		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
 		wget https://valtman.name/files/telegram-cli-1222
 		mv telegram-cli-1222 tgcli
 		chmod +x tgcli
 		cd ..
 		chmod +x bot
-		chmod +x tg
+}
+
+function print_logo() {
+	green "          ____  ____     _____"
+	green "         |  _ )|  _ \   |_   _|___ ____   __  __"
+	green "         |  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
+	green "         |____/|____/     |_|\____/\_____|_/\/\_|"
+	echo -e "\n\e[0m"
+}
+
+function logo_play() {
+    declare -A txtlogo
+    seconds="0.010"
+    txtlogo[1]=" ____  ____     _____"
+    txtlogo[2]="|  _ )|  _ \   |_   _|___ ____   __  __"
+    txtlogo[3]="|  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
+    txtlogo[4]="|____/|____/     |_|\____/\_____|_/\/\_|"
+    printf "\e[31m\t"
+    for i in ${!txtlogo[@]}; do
+        for x in `seq 0 ${#txtlogo[$i]}`; do
+            printf "${txtlogo[$i]:$x:1}"
+            sleep $seconds
+        done
+        printf "\n\t"
+    done
+    printf "\n"
+	echo -e "\e[0m"
+}
+
+function beyondteam() {
+	echo -e "\e[0m"
+	green "     >>>>                       We Are Not Attacker                             "
+	green "     >>>>                       We Are Not Alliance                             "
+	white "     >>>>                       We Are Programmer                               "
+	white "     >>>>                       We Are The Best                                 "
+	red   "     >>>>                       We Are Family                                   "
+	red   "     >>>>                       @BeyondTeam                                     "
+	echo -e "\e[0m"
+}
+
+red() {
+  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+}
+green() {
+  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+}
+white() {
+  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+}
+update() {
+	git pull
 }
 
 if [ "$1" = "install" ]; then
-  install
-  else
-
-if [ ! -f ./tg/tgcli ]; then
-    echo "tg not found"
+	print_logo
+	beyondteam
+	logo_play
+	install
+elif [ "$1" = "update" ]; then
+	logo_play
+	beyondteam
+	update
+else
+if [ ! -f ./libs/tgcli ]; then
+    echo "tgcli not found"
     echo "Run $0 install"
     exit 1
 fi
-   echo -e "\033[38;5;208m"
-   echo -e "     > @Delete_TM :D                  "
-   echo -e "                                              \033[0;00m"
-   echo -e "\e[36m"
-   ./tg/tgcli -s ./bot/bot.lua $@
+	print_logo
+	beyondteam
+	logo_play
+	#sudo service redis-server restart
+	./libs/tgcli -s ./bot/bot.lua $@
 fi
-
-# Now All Argument Support after ./delete.sh !
-#	Arguments :
-#			#			#			#			#			#			#			#			#			#
-#  --phone/-u                           specify username (would not be asked during authorization)
-#  --verbosity/-v                       increase verbosity (0-ERROR 1-WARNIN 2-NOTICE 3+-DEBUG-levels)
-#  --enable-msg-id/-N                   message num mode
-#  --config/-c                          config file name
-#  --profile/-p                         use specified profile
-#  --wait-dialog-list/-W                send dialog_list query and wait for answer before reading input
-#  --disable-colors/-C                  disable color output
-#  --disable-readline/-R                disable readline
-#  --alert/-A                           enable bell notifications
-#  --daemonize/-d                       daemon mode
-#  --logname/-L <log-name>              log file name
-#  --username/-U <user-name>            change uid after start
-#  --groupname/-G <group-name>          change gid after start
-#  --disable-output/-D                  disable output
-#  --tcp-port/-P <port>                 port to listen for input commands
-#  --udp-socket/-S <socket-name>        unix socket to create
-#  --exec/-e <commands>                 make commands end exit
-#  --disable-names/-I                   use user and chat IDs in updates instead of names
-#  --help/-h                            prints this help
-#  --accept-any-tcp                     accepts tcp connections from any src (only loopback by default)
-#  --disable-link-preview               disables server-side previews to links
-#  --bot/-b                             bot mode
-#  --json                               prints answers and values in json format
-#  --permanent-msg-ids                  use permanent msg ids
-#  --permanent-peer-ids                 use permanent peer ids
-#			#			#			#			#			#			#			#			#			#
-#Example To launch with second profile :
-# ./bang-tg.sh -p second-profile
-
-#			  OR
-
-# ./bang-tg --profile second-profile
